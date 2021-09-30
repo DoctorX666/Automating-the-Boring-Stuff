4efda46abd99a9c01c983a73a34d5dfb37e5dc7f
DoctorX666 <DoctorX666@users.noreply.github.com>
2021-09-30 16:36:49 -0400

    upadte

---
+++ chap01/hello.py
@@ -0,0 +1,8 @@
+print('Hello, world!')
+print('What is your name?')  # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName + '.')
+print('Your name contains ' + str(len(myName)) + ' characters.')
+print('What is your age?')  # ask for users' age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
