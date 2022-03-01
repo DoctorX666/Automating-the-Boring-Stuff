3e04415ebd81e97770678e42d17848a81548a959
DoctorX666 <DoctorX666@users.noreply.github.com>
2022-03-01 12:41:59 -0500

    commit

--- chap01/hello.py
+++ chap01/hello.py
@@ -5,4 +5,4 @@ print('It is good to meet you, ' + myName + '.')
 print('Your name contains ' + str(len(myName)) + ' characters.')
 print('What is your age?')  # ask for users' age
 myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print('You will be ' + str(int(myAge) + 1) + ' in one year.')
